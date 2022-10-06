209737274fd6114451135ca3591a15558aafbdc2
Doumbisss <doumbia77fode@gmail.com>
2022-10-06 13:29:56 +0000

    first

--- app.js
+++ app.js
@@ -20,56 +20,55 @@ const database = new Database();
 //   console.error(err)
 // });
 
-Person.create([
-    {name: 'Doumbia Fode',age:23,email:'fode77doumbia@gmail.com',contacts: ["0777880082","0789800862"],favoriteFoods : ["foutou","pizza","pates","garba"]},
+// Person.create([
+//     {name: 'Doumbia Fode',age:23,email:'fode77doumbia@gmail.com',contacts: ["0777880082","0789800862"],favoriteFoods : ["foutou","pizza","pates","garba"]},
 
-    {name: 'Kouyate Karim',age:30,email:'kouyate02@gmail.com',contacts: ["0100000"],favoriteFoods : ["Riz gras","sauce auber","pates"]},
+//     {name: 'Kouyate Karim',age:30,email:'kouyate02@gmail.com',contacts: ["0100000"],favoriteFoods : ["Riz gras","sauce auber","pates"]},
 
-    {name: 'Doumbia Siaka',age:19,email:'doumbia21siaka@gmail.com',contacts: ["0757860082"],favoriteFoods : ["foutou","pizza"]}
+//     {name: 'Doumbia Siaka',age:19,email:'doumbia21siaka@gmail.com',contacts: ["0757860082"],favoriteFoods : ["foutou","pizza"]},
 
-])
+//     {name: 'Doumbia Mamadou',age:30,email:'doumbia21Mamadou@gmail.com',contacts: ["0757860082"],favoriteFoods : ["foutou","pizza"]}
+
+// ])
 
 
 
 // Person.find().then((person) =>{
 //     console.log(person)
-// })       /////////
+// })       
 
 // Person.findOne({favoriteFoods : "pizza"}).then((person) =>{
 //     console.log(person)
-// })           /////////
+// })          
 
 
-// let id = '633ca97f2d84f8811886d2b5';
+// let id = "633ec7b1bf817dec2679768a";
 // Person.findById(id).then((person) => {
 //         console.log(person)
-// })    /////////////
+// })   
 
 
-// let id = '633dffd9863fc2fd308cced3';
-// Person.findOneAndUpdate({id}, { $push: { favoriteFoods: ['Hamburger']  } }).then((person)=>{
+// let id = "633ec7b1bf817dec26797688";
+// Person.findOneAndUpdate({id}, { $push: { favoriteFoods: 'Hamburger'} }).then((person)=>{
 //     console.log(person);
-// })    /////////
+// })    
+
+// let id = ""633ec7b1bf817dec2679768a"";
+// Person.findOneAndRemove(id).then((person) => {
+//         console.log(person)
+// })  
 
 
-// Person.findOneAndRemove(id,
-//     function (error, success) {
-//          if (error) {
-//              console.log(error);
-//          } else {
-//              console.log(success);
-//          }
-//      });   /////////  
-
-
-// let id = '633e03575da6a21a595970e7';
-// Person.remove({id}).then((person)=>{
+// let id = "633eceb1b85223bca1beac9f";
+// Person.remove({'_id' : id}).then((person)=>{
 //     console.log(person);
-// });   /////////////
+// }); 
 
 
-
-// let id = '33ca97f2d84f8811886d2b4';
-// Person.findByIdAndUpdate(id,age: { $gte: 18 },{ $push: { favoriteFoods: ['Hamburger']}}).then((person)=>{
+// Person.findOneAndUpdate({name :'DOUMBIA FODE'},{ age: 20 } ).then((person)=>{
 //     console.log(person);
+// })
+
+// Person.find({favoriteFoods : "pizza"}).sort({name :'asc'}).limit(2).select('-age').then((person)=>{
+//     console.log(person)
 // })
